a97a37aa2c64ea669814d7aa970985e0cf840d1d
Christopher Lamm <chris@theantichris.com>
2019-10-11 08:20:17 -0400

    refactoring

--- FindDuplicates.go
+++ FindDuplicates.go
@@ -9,8 +9,14 @@ import (
 // FindDuplicates reads input lines and prints out which are duplicated with the
 // nubmer of times.
 func FindDuplicates(input io.Reader, output io.Writer) {
-	scanner := bufio.NewScanner(input)
 	counts := make(map[string]int)
+
+	countLines(input, counts)
+	printLines(output, counts)
+}
+
+func countLines(f io.Reader, counts map[string]int) {
+	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
 		if scanner.Text() == "" {
@@ -19,7 +25,9 @@ func FindDuplicates(input io.Reader, output io.Writer) {
 
 		counts[scanner.Text()]++
 	}
+}
 
+func printLines(output io.Writer, counts map[string]int) {
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Fprintf(output, "%d\t%s\n", n, line)
